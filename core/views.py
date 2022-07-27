bf287b1cfd2df3d1a33e259f2cf74f996d1fec9f
LUCAS FERNANDES <060457@TCB.local>
2022-07-27 14:28:53 -0300

    Incluido templates

--- core/views.py
+++ core/views.py
@@ -6,15 +6,17 @@ from django.contrib import messages
 
 
 # Create your views here.
-#def index(request):
+# def index(request):
 #    return redirect('/agenda/')
 
 def login_user(request):
     return render(request, 'login.html')
 
+
 def logout_user(request):
     logout(request)
     return redirect('/')
+
 
 def submit_login(request):
     if request.POST:
@@ -28,11 +30,30 @@ def submit_login(request):
             messages.error(request, "Usuario ou senha invalidos")
     return redirect('/')
 
-@login_required(login_url='/login/')
 
+@login_required(login_url='/login/')
 def lista_eventos(request):
     user = request.user
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
-    dados = {'eventos' :evento}
+    dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
